eb0b906ae66e09bef97874ca11862a45d327f38b
Gabriel Allegretti <gallegretti@inf.ufrgs.br>
2019-06-08 18:21:17 -0300

    Add basic movement simulation

--- src/Simulation.cpp
+++ src/Simulation.cpp
@@ -1,5 +1,42 @@
 #include "Simulation.h"
 
-void Simulation::step()
+void Simulation::step(float delta)
 {
+	// 1: Update position
+	for (int i = 0; i < positions.size(); i++)
+	{
+		vec2f& position = positions[i];
+		vec2f& velocity = velocities[i];
+		// Handle map collision
+		if ((position.x < map_size) || (position.x > map_size))
+		{
+			velocity.x = 0.0f;
+		}
+		if ((position.y < map_size) || (position.y > map_size))
+		{
+			velocity.y = 0.0f;
+		}
+
+		// TODO: Decide direction
+		vec2f acceleration;
+
+		// Update velocity
+		velocity.x += acceleration.x * delta;
+		velocity.y += acceleration.y * delta;
+
+		// Limit velocity
+		float current_velocity = sqrt((velocity.x * velocity.x) + (velocity.y * velocity.y));
+		if (current_velocity > maximum_velocity)
+		{
+			current_velocity = maximum_velocity;
+		}
+		float factor = 1.0 / current_velocity;
+		velocity.x *= factor;
+		velocity.y *= factor;
+		
+		// Update position
+		position.x += acceleration.x * delta;
+		position.y += acceleration.y * delta;
+
+	}
 }
--- src/Simulation.h
+++ src/Simulation.h
@@ -1,8 +1,60 @@
 #pragma once
+#include <vector>
+
+struct Entity {
+	int id;
+	vec2f position;
+	vec2f velocity;
+	float size;
+};
+
+struct vec2f {
+	float x;
+	float y;
+};
+
+struct EntityActionResult {
+	vec2f movement;
+	bool divide;
+};
 
 class Simulation
 {
 public:
 
-	void step();
+	// Each entity
+	//  Decide movement, update velocity and position
+	// Each entity
+	//  If size > x, divide
+
+	EntityActionResult update_entity();
+
+	/*
+	Sobre a simulação:
+    -Os organismos vivem em um ambiente finito de duas dimensões
+    -Um organismo pode se alimentar de outro, caso seja maior e esteja sobre o outro
+    -Um organismo pode se alimentar de nutrientes (aparecem aleatoriamente no ambiente)
+    -Após atingir certo tamanho, um organismo de divide em dois
+    -Com o passar do tempo, organismos gastam energia e perdem tamanho
+    -Um organismo pode se mover em qualquer direção
+    Sobre a paralelização:
+    -Os organismos vão tomar ações, atualizar suas posições e velocidades ao mesmo tempo.
+    -Barreira de sincronização
+    -Atualização de colisões, reprodução e alimentação
+	*/
+
+	void step(float delta);
+
+	// std::vector<Entity> entities;
+
+	std::vector<bool> is_alive;
+	std::vector<vec2f> positions;
+	std::vector<vec2f> velocities;
+	std::vector<float> mass;
+	std::vector<float> sizes;
+
+
+	static constexpr float map_size = 1024;
+
+	static constexpr float maximum_velocity = 1.0f;
 };
